6abb5de6f8dc41ea90affa2d774c16bc4a62214e
ZoloMohan <mohanzolomon@gmail.com>
2019-07-29 01:05:58 +0530

    Updates

---
+++ precache-manifest.a9665393fbac29b655d7603958e757bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e853312dd1292c3115d6fd946c4aeba",
+    "url": "/card-dealer/index.html"
+  },
+  {
+    "revision": "ce4dab3f7225fafb4c8f",
+    "url": "/card-dealer/static/css/main.b2c19567.chunk.css"
+  },
+  {
+    "revision": "a840b3d05d4191b7cd21",
+    "url": "/card-dealer/static/js/2.50c5cb1c.chunk.js"
+  },
+  {
+    "revision": "ce4dab3f7225fafb4c8f",
+    "url": "/card-dealer/static/js/main.3ddd2c97.chunk.js"
+  },
+  {
+    "revision": "d66a86cc9e5775235318",
+    "url": "/card-dealer/static/js/runtime~main.c0b5c97c.js"
+  }
+]);
