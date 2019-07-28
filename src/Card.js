0b414b9234693eb4f1baf1449bd1d2917cf9e00d
ZoloMohan <mohanzolomon@gmail.com>
2019-07-29 00:02:35 +0530

    Stack Cards Images

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Card.css';
 
 export default class Card extends Component {
-    render() {
-        return (
-            <img src = {this.props.imageUrl} alt = {this.props.name} />
-        )
-    }
+  constructor(props){
+  super(props);
+    let angle = Math.random() * 90 - 45,
+      xPos = Math.random() * 40 - 20,
+      yPos = Math.random() * 40 - 20;
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+  }
+
+  render() {  
+    return (
+      <img 
+        style = {{
+          transform: this._transform
+        }}
+        className="Card" 
+        src = {this.props.imageUrl} 
+        alt = {this.props.name} 
+      />
+    )
+  }
 }
--- src/Deck.js
+++ src/Deck.js
@@ -1,54 +1,57 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Card from './Card';
+import './Deck.css'
 
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 export default class Deck extends Component {
 
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             deck: null,
-             drawn:[]
-        }
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       deck: null,
+       drawn:[]
     }
+  }
 
-    async componentDidMount(){
-        let deckResponse = await axios.get(`${API_BASE_URL}/new/shuffle/?deck_count=1`);
-        this.setState({deck: deckResponse.data, drawn: []})
-    }
+  async componentDidMount(){
+    let deckResponse = await axios.get(`${API_BASE_URL}/new/shuffle/?deck_count=1`);
+    this.setState({deck: deckResponse.data, drawn: []})
+  }
 
-    getCard = async () => {
-        try{
-            let cardResponse = await axios.get(`${API_BASE_URL}/${this.state.deck.deck_id}/draw/?count=1`);
-            if(!cardResponse.data.success)
-                throw new Error ('No more cards availabale in the deck!');
-            let card = cardResponse.data.cards[0];
-            this.setState(currentState => ({
-                drawn: [
-                    ...currentState.drawn,
-                    {
-                        id: card.code,
-                        image: card.image,
-                        name: `${card.suit} ${card.value}`
-                    }
-                ]
-            }))
-        }
-        catch(error){
-            alert(error);
-        }
+  getCard = async () => {
+    try{
+      let cardResponse = await axios.get(`${API_BASE_URL}/${this.state.deck.deck_id}/draw/?count=1`);
+      if(!cardResponse.data.success)
+        throw new Error ('No more cards availabale in the deck!');
+      let card = cardResponse.data.cards[0];
+      this.setState(currentState => ({
+        drawn: [
+          ...currentState.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.suit} ${card.value}`
+          }
+        ]
+      }))
     }
+    catch(error){
+      alert(error);
+    }
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Card Dealer</h1>
-                {this.state.drawn.map(card => <Card key={card.id} imageUrl = {card.image} name = {card.name} />)}
-                <button onClick = {this.getCard}>Get Card</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='Deck'>
+        <h1>Card Dealer</h1>
+        <div className='Deck-cards'>
+          {this.state.drawn.map(card => <Card key={card.id} imageUrl = {card.image} name = {card.name} />)}
+        </div>
+        <button onClick = {this.getCard}>Get Card</button>
+      </div>
+    )
+  }
 }
